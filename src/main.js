e09838dd2b49c5d6af48e69d5287bec7a9058ce0
Duc Tran <ductranvp@gmail.com>
2022-01-27 16:19:51 +0700

    add fontawesome

--- src/main.js
+++ src/main.js
@@ -1,17 +1,23 @@
+/* Core */
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+/* Libraries */
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import vi from "element-plus/es/locale/lang/vi";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
+/* Assets */
+import "./assets/icons/fontawesome.js";
 import "./assets/styles/index.scss";
 
-const app = createApp(App)
-  .use(store)
-  .use(router)
-  .use(ElementPlus, { locale: vi });
+const app = createApp(App);
+
+app.use(store).use(router).use(ElementPlus, { locale: vi });
+
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.mount("#app");
