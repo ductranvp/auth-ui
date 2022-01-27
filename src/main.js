005a9db00947f068de03b5b7a1c3b85bdab8c770
Duc Tran <ductranvp@gmail.com>
2022-01-27 15:43:31 +0700

    add element plus

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import vi from "element-plus/es/locale/lang/vi";
+
 import "./assets/styles/index.scss";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App)
+  .use(store)
+  .use(router)
+  .use(ElementPlus, { locale: vi });
+
+app.mount("#app");
