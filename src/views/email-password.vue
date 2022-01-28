151e3241d89f0f53fa2796a5df1a3b44437ebf42
Duc Tran <ductranvp@gmail.com>
2022-01-28 10:15:45 +0700

    add firebase, register/login with email password

---
+++ src/views/email-password.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-row>
+    <el-col :span="12" class="p-4">
+      <EmailPasswordRegister />
+    </el-col>
+    <el-col :span="12" class="p-4">
+      <EmailPasswordLogin />
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import EmailPasswordRegister from "../components/email-password/EmailPasswordRegister.vue";
+import EmailPasswordLogin from "../components/email-password/EmailPasswordLogin.vue";
+export default {
+  name: "EmailPassword",
+  components: { EmailPasswordLogin, EmailPasswordRegister },
+};
+</script>
